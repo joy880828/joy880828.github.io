1e75c857ee5d336dba248e8c75d618123a60e3d6
joy880828 <37370703+joy880828@users.noreply.github.com>
2022-07-30 11:19:10 +0800

    test
    test

---
+++ js/script.js
@@ -0,0 +1,96 @@
+var songNum = 16;
+
+$(document).ready(function() {
+
+    //binding 隨機播放checkbox
+    $('#randomPlay').change(() => onRandomPlayCheckChanged());
+
+    //generate mp3 player
+    for (var i = 1; i <= songNum; i++) {
+
+        var myAudio = document.createElement("audio");
+        myAudio.controls = "controls";
+        myAudio.id = "fromJS" + i;
+        myAudio.textContent = "hi";
+
+        var mySrc = document.createElement("source")
+        mySrc.src = "musics/music" + i + ".mp3";
+        mySrc.type = "audio/mpeg";
+        myAudio.appendChild(mySrc);
+
+        document.body.appendChild(myAudio);
+
+    }
+
+
+
+
+    //自動輪播
+    for (var i = 1; i <= songNum; i++) {
+        let nextSongID = 1;
+        let player = document.getElementById("fromJS" + i);
+        if (i >= songNum) {
+            nextSongID = 1;
+        } else {
+            nextSongID = i + 1;
+        }
+        player.onended = () => {
+            document.getElementById("fromJS" + nextSongID).play();
+        };
+    }
+});
+
+
+
+function onRandomPlayCheckChanged() {
+
+    if ($('#randomPlay').is(":checked")) {
+
+        for (var i = 1; i <= songNum; i++) {
+
+            let player = document.getElementById("fromJS" + i);
+
+            player.onended = () => {
+                let id = player.id;
+                let currNumber = id.substr(id.length - 1);
+                let rndNumber = parseInt(Math.random() * songNum + 1);
+                while (currNumber == rndNumber) {
+                    rndNumber = parseInt(Math.random() * songNum + 1);
+                }
+                document.getElementById("fromJS" + rndNumber).play();
+            };
+        }
+
+    } else {
+
+        for (var i = 1; i <= songNum; i++) {
+
+            let nextSongID = 1;
+            let player = document.getElementById("fromJS" + i);
+            if (i >= songNum) {
+                nextSongID = 1;
+            } else {
+                nextSongID = i + 1;
+            }
+
+            player.onended = () => {
+                document.getElementById("fromJS" + nextSongID).play();
+            };
+        }
+    }
+}
+
+
+
+function playNextSong() {
+
+    var num = 1;
+
+    while (document.getElementById("fromJS" + num)) {
+        let player = document.getElementById("fromJS" + num);
+        if (!player.paused) {
+            player.currentTime = player.duration;
+        }
+        num++;
+    }
+}
